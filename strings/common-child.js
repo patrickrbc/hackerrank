f1e10a737c3d581baa40a06e7f09d83c41a70f3e
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-26 13:41:48 -0300

    Add more tests

--- strings/common-child.js
+++ strings/common-child.js
@@ -1,31 +1,37 @@
 
 const assert = require('assert')
 
-function max (a, b) {
-  return a > b ? a : b
-}
-
-function commonChild (s1, s2) {
+function commonChild (x, y) {
 
   let count = 0
-  let results = new Array(s1.length + 1).fill(0)
-    .map(item =>(new Array(s2.length + 1).fill(0)))
+  let results = new Array(x.length + 1).fill(0)
+    .map(item =>(new Array(y.length + 1).fill(0)))
 
-  for (var i = 0, len = s1.length; i <= len; i++) {
-    for (var j = 0, len2 = s2.length; j <= len2; j++) {
+  for (var i = 0, len = x.length; i <= len; i++) {
+    for (var j = 0, len2 = y.length; j <= len2; j++) {
 
       if (!i || !j)
         results[i][j] = 0
-      else if (s1[i-1] === s2[j-1])
+      else if (x[i-1] === y[j-1])
         results[i][j] = 1 + results[i-1][j-1]
       else
-        results[i][j] = max(results[i-1][j], results[i][j-1])
+        results[i][j] = Math.max(results[i-1][j], results[i][j-1])
     }
   }
 
-  return results[i-1][j-1]
+  return results[x.length][y.length]
 }
 
 assert.equal(commonChild('SHINCHAN', 'NOHARAAA'), 3)
 assert.equal(commonChild('ABCDEF', 'FBDAMN'), 2)
 assert.equal(commonChild('HARRY', 'SALLY'), 2)
+assert.equal(commonChild('hackerranks', 'hackers'), 7)
+assert.equal(commonChild('hacker', 'HACKERhka'), 2)
+assert.equal(commonChild('abcd', 'abdc'), 3)
+assert.equal(commonChild('abc', 'aedace'), 2)
+assert.equal(commonChild('', 'a'), 0)
+assert.equal(commonChild('a', ''), 0)
+assert.equal(commonChild('-1', ''), 0)
+assert.equal(commonChild('axaaabaa3a', 'aaaa'), 4)
+assert.equal(commonChild('abc', 'cbac'), 2)
+assert.equal(commonChild('AGGTAB', 'GXTXAYB'), 4)
